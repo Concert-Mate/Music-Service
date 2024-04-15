babe57a8344a5b8c33782cd837321dd432a53df9
Kirill Kondrenko <kondrenkokp@yandex.ru>
2024-04-16 01:00:50 +0700

    Добавил кэширование с помощью redis

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+import asyncio
+
 from fastapi import FastAPI, HTTPException, status
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+from redis import asyncio as aioredis, Redis
 
 from model import Concert, TracksList
 from services import NotFoundException, InternalServiceErrorException
 from services import YandexMusicService
-
-
-async def on_startup() -> None:
-    await yandex_music_service.setup()
-
-
-async def on_shutdown() -> None:
-    await yandex_music_service.terminate()
-
+from settings import settings
 
 yandex_music_service = YandexMusicService()
-app: FastAPI = FastAPI(
-    on_startup=[on_startup],
-    on_shutdown=[on_shutdown]
-)
+app: FastAPI = FastAPI()
+redis: Redis = aioredis.from_url(f'redis://:{settings.redis_password}@{settings.redis_host}:{settings.redis_port}')
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    await yandex_music_service.setup()
+
+    await redis.ping()  # Check redis connection and auth
+    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    await yandex_music_service.terminate()
+    redis.close()
 
 
 @app.get('/concerts')
+@cache(expire=60)
 async def get_concerts(artist_id: int) -> list[Concert]:
     try:
         return await yandex_music_service.parse_concerts(artist_id)
@@ -31,8 +42,10 @@ async def get_concerts(artist_id: int) -> list[Concert]:
 
 
 @app.get('/tracks-lists')
+@cache(expire=600)
 async def get_tracks_list_info(url: str) -> TracksList:
     try:
+        await asyncio.sleep(10)
         return await yandex_music_service.parse_tracks_list(url)
     except NotFoundException as e:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
---
+++ settings.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    redis_host: str = 'localhost'
+    redis_port: int = 6379
+    redis_password: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
